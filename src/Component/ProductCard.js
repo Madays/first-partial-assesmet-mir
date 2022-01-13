d8767e3c19e37c023bd55b2b6665d29b42f0d740
madays <maday.choque@gmail.com>
2022-01-12 22:55:57 -0500

    card component

---
+++ src/Component/ProductCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ProductCard extends React.Component {    
+  render(){
+    return <div>
+      <img src=""/>
+      <p>Lorem ipson</p>
+      <div> 
+        <span>00:32:52</span>
+        <button>Go To detail</button>  
+      </div>
+    </div>
+  }
+}
+
+export default ProductCard;
